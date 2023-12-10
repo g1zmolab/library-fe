0c519a2f3b0efce95d4bb4793a44304827d8a67b
accel <cyber_accel2089@pm.me>
2023-12-10 21:16:17 +0200

    remove unused files

--- src/lib/stores/mobile-menu.store.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const showMobileMenu = writable(false);
-
-export const toggleMobileMenu = () => showMobileMenu.update((isOpen) => (isOpen = !isOpen));
