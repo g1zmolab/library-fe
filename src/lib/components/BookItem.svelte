e111601445f22fe06b1dd524270269dd576cd2b9
accel <cyber_accel2089@pm.me>
2023-12-11 01:49:16 +0200

    improve on mobile devices

--- src/lib/components/BookItem.svelte
+++ src/lib/components/BookItem.svelte
@@ -4,13 +4,8 @@
 
 <div class="border-t border-t-zinc-500 py-3">
   <div class="pb-1">
-    <span class="text-xl font-bold">{book.attributes.title}</span>
-    <span class="text-gray-200">{book.attributes.yearPublished}</span>
-    {#if book.attributes.publisher.data}
-      <span class="float-right">
-        {book.attributes.publisher.data.attributes.name}
-      </span>
-    {/if}
+    <span class="sm:text-xl font-bold">{book.attributes.title}</span>
+    <span class="text-gray-200 italic">({book.attributes.yearPublished})</span>
   </div>
   <div>
     {#each book.attributes.authors.data as author, i}
@@ -20,14 +15,17 @@
       {/if}
     {/each}
   </div>
-  {#if book.attributes.quantities}
-    <div class="text-gray-300 italic">
-      {#each book.attributes.quantities.data as quantity, i}
-        <span>{quantity.attributes.library.data.attributes.name}</span>
-        {#if i < book.attributes.quantities.data.length - 1}
-          <span>, </span>
-        {/if}
-      {/each}
+  <div >
+    <span>
+      {book.attributes.publisher.data.attributes.name}
+    </span>
+    <div class="sm:float-right italic text-gray-300">
+      {#if book.attributes.quantities.data.length === 1}
+        {book.attributes.quantities.data[0].attributes.library.data.attributes
+          .name}
+      {:else}
+        σε {book.attributes.quantities.data.length} βιβλιοθήκες
+      {/if}
     </div>
-  {/if}
+  </div>
 </div>
