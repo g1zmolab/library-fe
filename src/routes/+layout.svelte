6032790427a1a53dfdb1bf206dda230928f6d109
accel <cyber_accel2089@pm.me>
2023-12-11 02:08:48 +0200

    fix header on mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,15 +4,15 @@
 
 <header class="bg-zinc-800 text-fuchsia-300">
   <div
-    class="mx-auto max-w-4xl py-6 sm:px-6 lg:px-8 flex items-center justify-between"
+    class="py-3 mx-auto max-w-4xl sm:px-6 lg:px-8 flex items-center justify-between"
   >
-    <div>
+    <div class="mx-4 sm:mx-0">
       <a href="/">
         <div class="text-4xl font-bold mb-2">gizmolib</div>
-        <div class="italic">Ψηφιακός Κατάλογος Κινηματικών Βιβλιoθηκών</div>
+        <div class="italic text-sm">Ψηφιακός Κατάλογος Κινηματικών Βιβλιoθηκών</div>
       </a>
     </div>
-    <div>
+    <div class="hidden sm:block">
       <a href="/about" class="text-xl px-2">Σχετικά</a>
     </div>
   </div>
